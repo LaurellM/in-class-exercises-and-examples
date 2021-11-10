29e76d22887146332a6e2b29d4f0a00840460ee7
LaurellM <laurellmurray@gmail.com>
2021-11-10 18:58:32 -0500

    Changed file name

--- ELM_draw_turtle.py
+++ ELM_draw_turtle.py
@@ -1,1 +1,14 @@
 # Instructions for this exercise are here https://realpython.com/beginners-guide-python-turtle/
+
+print("This is my turtle program")
+import turtle
+s = turtle.getscreen()
+t = turtle.Turtle()
+t.circle(90)
+turtle.mainloop()
+
+
+
+
+
+
